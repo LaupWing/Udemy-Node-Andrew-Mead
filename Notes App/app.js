19c63bb525bc46d625aa343f4bf927db2cf007f4
LaupWing <loc_strict@hotmail.com>
2021-03-15 14:11:57 +0100

    update

--- Notes App/app.js
+++ Notes App/app.js
@@ -2,12 +2,24 @@ const yargs = require('yargs')
 
 yargs.version('1.1.0')
 
-console.log(yargs.argv)
 yargs.command({
    command: 'add',
    describe: 'Add a new note',
-   handler: function(){
-      console.log('Adding a new note')
+   builder:{
+      title: {
+         describe: 'Note title',
+         demandOption: true,
+         type: 'string'
+      },
+      body: {
+         describe: 'Notes body',
+         demandOption: true,
+         type: 'string'
+      },
+   },
+   handler: function(argv){
+      console.log('Adding a new note', argv.title)
+      console.log('Body', argv.body)
    }
 })
 yargs.command({
@@ -18,9 +30,19 @@ yargs.command({
    }
 })
 yargs.command({
-   command: 'add',
-   describe: 'Add a new note',
+   command: 'read',
+   describe: 'Read a new note',
    handler: function(){
-      console.log('Adding a new note')
+      console.log('Reading a note')
    }
 })
+yargs.command({
+   command: 'list',
+   describe: 'List a new note',
+   handler: function(){
+      console.log('Listing all notes')
+   }
+})
+
+// Goes through the arguemnts
+yargs.parse()
