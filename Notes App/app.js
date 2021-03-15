d3e68616ac0a8634354ea16e0c02052eab61a25b
LaupWing <loc_strict@hotmail.com>
2021-03-15 14:03:13 +0100

    16

--- Notes App/app.js
+++ Notes App/app.js
@@ -1,5 +1,26 @@
-const fs = require('fs')
+const yargs = require('yargs')
 
-fs.writeFileSync('notes.txt', 'test')
-fs.appendFileSync('notes.txt', 'test')
+yargs.version('1.1.0')
 
+console.log(yargs.argv)
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   handler: function(){
+      console.log('Adding a new note')
+   }
+})
+yargs.command({
+   command: 'remove',
+   describe: 'Remove a new note',
+   handler: function(){
+      console.log('Removing a new note')
+   }
+})
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   handler: function(){
+      console.log('Adding a new note')
+   }
+})
