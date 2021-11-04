85c0c8bb9e20c5c3ca4c4c811255a38a6674ed5e
LaupWing <loc_strict@hotmail.com>
2021-11-04 16:26:54 +0100

    16

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,22 @@
-const chalk = require('chalk')
+const yargs = require('yargs')
 
+yargs.version('1.1.0')
 
-console.log(chalk.green.bold('Hai'))
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   handler: function(){
+      console.log('Adding a new note')
+   }
+})
+
+yargs.command({
+   command: 'Remove',
+   describe: 'Remove a note',
+   handler: function(){
+      console.log('Removing the note')
+   }
+})
+
+// const command = process.argv[2]
+
