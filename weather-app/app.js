993bdd4c746d422d662012372b68593ad646cc61
LaupWing <loc_strict@hotmail.com>
2021-11-26 13:05:22 +0100

    31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,16 @@
 const request = require('postman-request')
 
-const url = ''
+const url = 'http://api.weatherstack.com/current?access_key=4615109163fe77c2b45556b80acf8957&query=37.8267,-122.4233'
 
-request
-   .get(url)
-   .onRequestError((error)=>{
-      console.log(error)
-   })
-   .onRequestResponse((response)=>{
-      console.log(response)
-   })
+// request
+//    .get(url)
+//    .onRequestError((error)=>{
+//       console.log(error)
+//    })
+//    .onRequestResponse((response)=>{
+//       console.log(response)
+//    })
+
+request({url}, (err, res)=>{
+   console.log(JSON.parse(res.body))
+})
