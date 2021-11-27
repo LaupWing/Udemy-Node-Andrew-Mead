f2a99f889ee1f0f1c6329b1eb1508ba56e4ffaad
LaupWing <loc_strict@hotmail.com>
2021-11-27 13:27:11 +0100

    13

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,10 @@
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=4615109163fe77c2b45556b80acf8957&query=37.8267,-122.4233'
+const weather_url = 'http://api.weatherstack.com/current?access_key=4615109163fe77c2b45556b80acf8957&query=37.8267,-122.4233'
+const location_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Heiloo.json?access_token=pk.eyJ1IjoibGF1cHdpbmciLCJhIjoiY2sxY2F3NXkyMDJhNTNjbjl6aGhzOGdicyJ9.2YhNrtIiq0F6biulYD9N2g&limit=1'
 
 // request
-//    .get(url)
+//    .get(weather_url)
 //    .onRequestError((error)=>{
 //       console.log(error)
 //    })
@@ -11,7 +12,14 @@ const url = 'http://api.weatherstack.com/current?access_key=4615109163fe77c2b455
 //       console.log(response)
 //    })
 
-request({url}, (err, res)=>{
+request({url:weather_url}, (err, res)=>{
    const data = JSON.parse(res.body)
-   console.log(data.current)
+   // console.log(data.current)
+})
+request({url: location_url}, (err, res)=>{
+   const data = JSON.parse(res.body) 
+   const [long, lat] = data.features[0].center
+   console.log(long, lat)
+   // const data = JSON.parse(res.body)
+   // console.log(data)
 })
